f533e025d454b446aeaf7abbafd25549dfa1172f
Stephen McBride <stevemcbride3@gmail.com>
2020-04-22 18:36:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,8 @@ class String
 
   def count_sentences
     split_sentence = ["#{self}"]
-    [".", "?", "!"].each do
-      split_sentence.map! {|s| self.split("!")}.flatten
+    [".", "?", "!"].each do |symbol|
+      split_sentence.map! {|s| self.split(symbol)}.flatten
     end
     split_sentence.delete_if { |s| s.empty? }.length
   end
