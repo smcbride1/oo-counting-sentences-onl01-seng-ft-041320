0e51cdeedf30ce8875a2733dd90f395f3dec1b0f
Stephen McBride <stevemcbride3@gmail.com>
2020-04-22 18:40:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = ["#{self}"]
+    [".", "?", "!"].each do |symbl|
+      split_sentence.map! {|s| s.split(symbl)}.flatten!
+    end
+    split_sentence.delete_if { |s| s.empty? }.length
   end
+
 end
